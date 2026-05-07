1a53621d5139afa8899669efddec3722acc46129
NJamsin <liteul@Noah.localdomain>
2026-05-07 17:54:34 +0200

    Typo fix dagman

--- src/peket/gw_search/setup_sig_pipeline.py
+++ src/peket/gw_search/setup_sig_pipeline.py
@@ -97,7 +97,8 @@ JOB SIG_PREP {os.path.join(sub_files_dir, "sig_prep.sub")}
 JOB SIG_SEARCH {sig_search_sub}
 JOB SIG_POST {os.path.join(sub_files_dir, "sig_post.sub")}
 
-RETRY SEARCH 3 # Retry the search node up to 3 times if it fails, as it can be the most resource-intensive part
+# Retry the search node up to 3 times if it fails, as it can be the most resource-intensive part
+RETRY SEARCH 3 
 
 # Pass the config file path dynamically
 VARS SIG_PREP config="{config_path}"
